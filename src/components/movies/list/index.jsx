969417406591cfd10db4d5ae60bf71081040bdbf
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-02-11 19:01:37 -0600

    Ad Styles to list of movies

--- src/components/movies/list/index.jsx
+++ src/components/movies/list/index.jsx
@@ -4,6 +4,7 @@
 // importamos Component para hacer un componente de clase
 import React, { Component } from "react"
 import moment from "moment/moment"
+import { RingLoader } from "react-spinners"
 
 // importamos nuestros servicios 
 import { getMoviesService } from "../../../services/"
@@ -48,20 +49,22 @@ export default class MovieList extends Component {
         // Cargamos el componente segun se necesita basandonos en el estado de la respuesta del API
         return (
             <>
-                <p>La lista de peliculas:</p>
-                {
-                    isReady ? 
-                        <ListComponent
-                            movies={movies}
-                        />
-                        :
-                        hasError ?
-                            <ErrorComponent
-                                error={error}
+                <div className="principal-container">
+                    <p className="page-title">La lista de peliculas:</p>
+                    {
+                        isReady ? 
+                            <ListComponent
+                                movies={movies}
                             />
-                            : <LoadingComponent/> 
-                            
-                }
+                            :
+                            hasError ?
+                                <ErrorComponent
+                                    error={error}
+                                />
+                                : <LoadingComponent/> 
+                                
+                    }
+                </div>
             </>
         )
     }
@@ -74,60 +77,63 @@ export default class MovieList extends Component {
 // Componente para listar las peliculas que hay registradas
 const ListComponent = (props) => (
     <>
-        {
-         // Revisamos que si hayan peliculas existentes   
-         props.movies.length > 0 ?
-            props.movies.map((movie) => (
-                <MovieCard key={movie._id}
-                    movie={movie}
-                />
-            ))
-            : <p>No hay peliculas registradas aun</p>
-        }
+        <div className="movie-card-container">
+            {
+            // Revisamos que si hayan peliculas existentes   
+            props.movies.length > 0 ?
+                props.movies.map((movie) => (
+                    <MovieCard key={movie._id}
+                        movie={movie}
+                    />
+                ))
+                : <p className="info-parragraf">No hay peliculas registradas aun</p>
+            }
+        </div>
     </>
 )
 
 // Componente de pelicula individual
 const MovieCard = ({movie}) => (
     <>
-        <div className="movie-card-container">
-            <div className="movie-card-info">
-                <div className="movie-card-important">
-                    <p className="movie-card-title">{movie.title}</p>
-                    <p className="movie-card-description">{movie.description}</p>
-                </div>
-                <div className="movie-card-detail">
-                    <p>
-                        Costo de la entrada: <span>{movie.tiketPrice}</span>
-                    </p>
-                    <p>
-                        Duracion de la pelicula: <span>{movie.duration} mins</span>
-                    </p>
-                    <p>
-                        {
-                            movie.isOnCinemas ? 
-                                'Disponible en cines'
-                                : 'No disponible en cines'
-                        }
-                    </p>
-                </div>
-                <div className="movie-card-schedules">
+        <div className="movie-card">
+            <div className="movie-card-important">
+                <p className="movie-card-title">{movie.title}</p>
+                <p className="movie-card-description">{movie.description}</p>
+            </div>
+            <div className="movie-card-detail">
+                <p>
+                    Costo de la entrada: <span>{movie.tiketPrice}</span>
+                </p>
+                <p>
+                    Duracion de la pelicula: <span>{movie.duration} </span>mins
+                </p>
+                <p>
                     {
-                        movie.schedules.length > 0 ?
-                            <>
-                            <p>Horarios disponibles: </p>
-                            {
-                                movie.schedules.map((schedule) => (
-                                    <p key={schedule._id}>{moment(schedule.time).format('DD / MM - HH:mm')}</p>
-                                ))
-                            }
-                            </>
-                            : <p>No hay horarios disponibles</p>
+                        movie.isOnCinemas ? 
+                            'Disponible en cines'
+                            : 'No disponible en cines'
                     }
-                </div>
+                </p>
+            </div>
+            <div className="movie-card-schedules">
+                <hr />
+                {
+                    movie.schedules.length > 0 ?
+                        <>
+                        <p>Horarios disponibles: </p>
+                        {
+                            movie.schedules.map((schedule, index) => (
+                                <span key={schedule._id}>
+                                {index > 0 && " | "}
+                                {moment(schedule.time).format('DD / MM - HH:mm')}
+                            </span>
+                            ))
+                        }
+                        </>
+                        : <p>No hay horarios disponibles</p>
+                }
             </div>
         </div>
-        <hr />
     </>
 )
 
@@ -135,8 +141,8 @@ const MovieCard = ({movie}) => (
 // Componente para mostrar los errores
 const ErrorComponent = ({error}) => (
     <>
-        <p>Hubo un error inesperado :c</p>
-        <p> -{'>'} {error}</p>
+        <p className="info-parragraf">Hubo un error inesperado :c</p>
+        <p className="info-parragraf"> -{'>'} {error}</p>
     </>
 )
 
@@ -144,6 +150,7 @@ const ErrorComponent = ({error}) => (
 // Componente que se muestra mientras se cargan las peliculas
 const LoadingComponent = (props) => (
     <>
-        <p>Obteniendo peliculas...</p>
+        <p className="info-parragraf">Obteniendo peliculas...</p>
+        <RingLoader color="#ffffff" size={100}/>
     </>
 )
